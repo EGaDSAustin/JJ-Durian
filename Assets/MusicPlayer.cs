2cd93f7b91d9cb4a835a41e5f4e4b1ae37460251
Gavin Hughes <49838674+RedstoneEngine@users.noreply.github.com>
2023-12-18 17:59:07 -0600

    MusicPlayer

--- Assets/MusicPlayer.cs
+++ Assets/MusicPlayer.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class MusicPlayer : MonoBehaviour
 {
+    public AudioClip clip;
+
+    AudioSource source;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        source = GetComponent<AudioSource>();
+        StartCoroutine(loopMusic());
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator loopMusic()
     {
-        
+        yield return new WaitUntil(() => !source.isPlaying);
+        source.clip = clip;
+        source.loop = true;
+        source.Play();
     }
 }
